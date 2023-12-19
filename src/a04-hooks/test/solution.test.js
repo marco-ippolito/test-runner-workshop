73643bcf9630338ad7d056635f09f06ab2e4e6ce
marco-ippolito <marcoippolito54@gmail.com>
2023-12-19 10:29:22 +0100

    feat: exercise 4

--- src/a04-hooks/test/solution.test.js
+++ src/a04-hooks/test/solution.test.js
@@ -1,33 +1,39 @@
 import assert from "node:assert";
-import { test } from "node:test";
-import { authenticateUser, closeDatabaseConnection, connectToDatabase, createUser, deleteUser } from '../src/index.js';
+import { test, before, beforeEach, afterEach, after } from "node:test";
+import { connectToDatabase, closeDatabaseConnection, createUser, authenticateUser, deleteUser } from '../src/index.js';
 
 let databaseConnection;
 let user;
 
-databaseConnection = await connectToDatabase();
+before(async () => {
+    databaseConnection = await connectToDatabase();
+});
+
+after(async () => {
+    await closeDatabaseConnection(databaseConnection);
+});
+
+beforeEach(async () => {
+    user = await createUser(databaseConnection, 'testuser', 'password123');
+});
+
+afterEach(async () => {
+    await deleteUser(databaseConnection, user);
+});
 
 test('Authentication Module Tests', async () => {
     await test('should authenticate a valid user', async () => {
-        user = await createUser(databaseConnection, 'testuser', 'password123');
         const result = await authenticateUser(databaseConnection, 'testuser', 'password123');
         assert.strictEqual(result, true, 'Authentication should succeed');
-        await deleteUser(databaseConnection, user);
     });
 
     await test('should reject invalid password', async () => {
-        user = await createUser(databaseConnection, 'testuser', 'password123');
         const result = await authenticateUser(databaseConnection, 'testuser', 'wrongpassword');
         assert.strictEqual(result, false, 'Authentication should fail with incorrect password');
-        await deleteUser(databaseConnection, user);
     });
 
     await test('should reject non-existing user', async () => {
-        user = await createUser(databaseConnection, 'testuser', 'password123');
         const result = await authenticateUser(databaseConnection, 'nonexistentuser', 'password123');
         assert.strictEqual(result, false, 'Authentication should fail with non-existing user');
-        await deleteUser(databaseConnection, user);
     });
 });
-
-await closeDatabaseConnection(databaseConnection);
